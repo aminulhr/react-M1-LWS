109516eeb0a387b0b9d69bfbebc69117672a4eb7
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-12-04 23:45:21 +0600

    new

---
+++ src/new.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const new = () => {
+    return (
+        <div>
+            <p>lorem20</p>
+        </div>
+    );
+};
+
+export default new;
--- src/text2.jsx
+++ src/text2.jsx
@@ -2,6 +2,7 @@ export default function () {
   return (
     <>
       <h1>Text Rectj</h1>
+      <h2>Add more</h2>
     </>
   );
 }

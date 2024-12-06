227fca4541982f1945e009baeb0121121e695650
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2024-12-06 23:46:15 +0600

    bad effect

--- src/new.jsx
+++ src/new.jsx
@@ -5,8 +5,9 @@ const new = () => {
         <div>
             <p>lorem20</p>
             <h1> 3 days no work</h1>
+            <h1>4 days gone but i can not code , this is vary bad effect for my life</h1>
         </div>
-    );
+     );
 };
 
 export default new;
